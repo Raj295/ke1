76199f93c38edd908f1399731115713b35372067
Raj295 <51864196+Raj295@users.noreply.github.com>
2020-11-14 09:42:23 +0000

    Update wsgi.py

--- djecommerce/wsgi.py
+++ djecommerce/wsgi.py
@@ -1,7 +1,15 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djecommerce.settings')
 
 application = get_wsgi_application()
+
+if not settings.DEBUG:
+    try:
+        from dj_static import Cling
+        application = Cling(get_wsgi_application())
+    except:
+        pass
